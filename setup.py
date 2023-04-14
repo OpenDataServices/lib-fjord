70dd1a55da194aafdee8e51710fe9bc4efd8ec0e
James B <james.baster@opendataservices.coop>
2023-04-14 15:42:47 +0100

    Start

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="libcove2",
+    version="0.0.0",
+    author="Open Data Services",
+    author_email="code@opendataservices.coop",
+    url="https://github.com/OpenDataServices/lib-cove-2",
+    description="A data review library",
+    packages=find_packages(),
+    long_description="A data review library",
+    python_requires=">=3.8",
+    install_requires=[
+    ],
+    classifiers=[
+    ],
+)
+
