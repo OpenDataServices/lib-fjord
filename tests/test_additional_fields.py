3cbc23ed8a1b56c957675bf579a2fb4588e52c28
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-09-22 18:43:24 +0100

    libcove2: Improve code formatting

--- tests/test_additional_fields.py
+++ tests/test_additional_fields.py
@@ -4,11 +4,13 @@ import pytest
 from libcove2.common import get_additional_fields_info
 from libcove2.common import schema_dict_fields_generator
 
+
 @pytest.fixture
 def bods_data_0_3():
     """Data for BODS 0.3"""
     with open("tests/fixtures/bods-data-0-3-0.json", "r") as read_file:
         return json.load(read_file)
+
 
 @pytest.fixture
 def schema_fields_0_3():
@@ -16,11 +18,13 @@ def schema_fields_0_3():
     with open("tests/fixtures/schema-0-3-0.json", "r") as read_file:
         return set(schema_dict_fields_generator(json.load(read_file)))
 
+
 @pytest.fixture
 def bods_data_0_4():
     """Data for BODS 0.4"""
     with open("tests/fixtures/bods-data-0-4-0.json", "r") as read_file:
         return json.load(read_file)
+
 
 @pytest.fixture
 def schema_fields_0_4():
@@ -28,11 +32,13 @@ def schema_fields_0_4():
     with open("tests/fixtures/schema-0-4-0.json", "r") as read_file:
         return set(schema_dict_fields_generator(json.load(read_file)))
 
+
 def test_additional_fields_0_3(bods_data_0_3, schema_fields_0_3):
 
     additional_fields = get_additional_fields_info(bods_data_0_3, schema_fields_0_3)
 
     assert len(additional_fields) == 0
+
 
 def test_additional_fields_0_4(bods_data_0_4, schema_fields_0_4):
     print(schema_fields_0_4)
